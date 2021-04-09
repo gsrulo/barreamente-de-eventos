a826e65d10a4dd9be7c4d96f8243171868818d15
Gabriel Santos Rulo <55097727+GSRulo@users.noreply.github.com>
2021-04-08 21:51:29 -0300

    Atualização do Micro Serviço

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.post("/eventos", (req, res) => {
-    console.log(req.body);
+app.post("/eventos", async(req, res) => {
+    const evento = req.body;
+
+    //envia o evento para o microsserviço de lembretes
+    await axios.post("http://localhost:3000/eventos", evento);
+    //envia o evento para o microsserviço de observações
+    await axios.post("http://localhost:4000/eventos", evento);
+    //envia o evento para o microsserviço de consulta
+    await axios.post("http://localhost:5000/eventos", evento);
+
     res.status(200).send({
         msg: "OK"
     });
 });
+
 
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000.')

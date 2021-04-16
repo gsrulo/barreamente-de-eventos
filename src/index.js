e6626f05fd9d8385f9bd8e02186201ed04c5a73c
Gabriel Santos Rulo <55097727+GSRulo@users.noreply.github.com>
2021-04-16 19:35:37 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ app.post("/eventos", async(req, res) => {
     const evento = req.body;
 
     //envia o evento para o microsserviço de lembretes
-    await axios.post("http://localhost:3000/eventos", evento);
+    axios.post("http://localhost:3000/eventos", evento);
     //envia o evento para o microsserviço de observações
-    await axios.post("http://localhost:4000/eventos", evento);
+    axios.post("http://localhost:4000/eventos", evento);
     //envia o evento para o microsserviço de consulta
-    await axios.post("http://localhost:5000/eventos", evento);
+    axios.post("http://localhost:5000/eventos", evento);
+    //envia o evento para o microsserviço de classificação
+    axios.post("http://localhost:7000/eventos", evento);
 
     res.status(200).send({
         msg: "OK"

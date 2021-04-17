f4f91021049bbb5bb0ad00f0214b58a5f1671d4d
Gabriel Santos Rulo <55097727+GSRulo@users.noreply.github.com>
2021-04-16 21:09:43 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.post("/eventos", async(req, res) => {
-    const evento = req.body;
+const eventos = []
 
+app.post("/eventos", async (req, res) => {
+    const evento = req.body;
+    eventos.push(evento)
     //envia o evento para o microsserviço de lembretes
-    axios.post("http://localhost:3000/eventos", evento);
-    //envia o evento para o microsserviço de observações
     axios.post("http://localhost:4000/eventos", evento);
-    //envia o evento para o microsserviço de consulta
+    //envia o evento para o microsserviço de observações
     axios.post("http://localhost:5000/eventos", evento);
+    //envia o evento para o microsserviço de consulta
+    axios.post("http://localhost:6000/eventos", evento);
     //envia o evento para o microsserviço de classificação
     axios.post("http://localhost:7000/eventos", evento);
 
@@ -21,6 +23,9 @@ app.post("/eventos", async(req, res) => {
     });
 });
 
+app.get('/eventos', (req, res) => {
+    res.send(eventos);
+})
 
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000.')
